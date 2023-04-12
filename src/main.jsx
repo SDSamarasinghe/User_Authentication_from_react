c23bf72cf080eaae398778db5a2cd5a8b015d3dc
lksad <it20605902@my.sliit.lk>
2023-04-12 22:20:27 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-tso7e4vry35ouuf2.us.auth0.com"
+      clientId="PBqljkjphpEnMXDSd4N8CDx0y4GZ4vws"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
